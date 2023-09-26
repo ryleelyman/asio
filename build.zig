a452e80b0ddb622cc3b0aa4f9cbd3639a169b264
Rylee Lyman <rylee.alanza@gmail.com>
2023-09-26 18:55:25 -0400

    feat: add build.zig

---
+++ build.zig
@@ -0,0 +1,27 @@
+const std = @import("std");
+
+pub fn build(b: *std.Build) void {
+    const target = b.standardTargetOptions(.{});
+    const optimize = b.standardOptimizeOption(.{});
+
+    const asio = b.addStaticLibrary(.{
+        .name = "asio",
+        .target = target,
+        .optimize = optimize,
+    });
+
+    asio.installHeader(b.pathJoin(&.{ "asio", "include", "asio.hpp" }), "asio.hpp");
+    asio.installHeader(
+        b.pathJoin(&.{ "asio", "include", "asio", "system_timer.hpp" }),
+        b.pathJoin(&.{ "asio", "system_timer.hpp" }),
+    );
+    asio.defineCMacro("ASIO_STANDALONE", "1");
+    asio.defineCMacro("ASIO_SEPARATE_COMPILATION", "1");
+    asio.addCSourceFile(.{
+        .file = .{ .path = b.pathJoin(&.{ "asio", "src", "asio.cpp" }) },
+        .flags = &.{"-Wno-deprecated-declarations"},
+    });
+    asio.addIncludePath(.{ .path = b.pathJoin(&.{ "asio", "include" }) });
+    asio.linkLibCpp();
+    b.installArtifact(asio);
+}
